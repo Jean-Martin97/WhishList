2c15c92fbc6b88d554459ba45d4f1518179504a6
Jean Martin <jean.martin@student.hepl.be>
2022-11-21 17:53:17 +0100

    Update products plus link in name

--- app.js
+++ app.js
@@ -1,32 +1,28 @@
 const PRODUCTS = [
-    {category: "Photographie", price: "400 €", name: "Objectif Nikkor 85mm", rate: "4.8", lien: "https://www.fr.fnac.be/Objectif-Reflex-Nikon-AF-S-FX-85mm-f-1-8-Nikkor-Serie-G/a4112365"},
-    {category: "Photographie", price: "210 €", name: "Objectif Nikkor 50mm", rate: "4.5", lien: "https://www.amazon.fr/dp/B004Y1AYAC/ref=asc_df_B004Y1AYAC1639512000000/?tag=ledenicheur-amazon-fr-21&creative=22686&creativeASIN=B004Y1AYAC&linkCode=df0&smid=A1X6FK5RDHNB96&th=1"},
-    {category: "Photographie", price: "169 €", name: "Objectif Nikkor 35mm", rate: "4.3", lien: "https://www.mediamarkt.be/fr/product/_nikon-objectif-standard-af-s-dx-nikkor-35mm-f1-8g-jaa132da-1094649.html?gclid=Cj0KCQiAzMGNBhCyARIsANpUkzOpWJZO6vbZJx6fU7zwCm87-zoDKQFAYf6HRoYWwGcF7DMdUUIlSIgaAgfEEALw_wcB&gclsrc=aw.ds"},
-    {category: "Photographie", price: "115 €", name: "Flash pour appareil photo millieu de gamme", rate: "4.5", lien: "https://www.photogalerie.com/flash-speedlite-yongnuo-yn-568ex-iii-pour-nikon?gclid=CjwKCAjwoP6LBhBlEiwAvCcthBMBmEE3jH5iKWEGHmToAoww86tWUCJTp2EOvifsz3TqQ-3lTZ9n4xoCcvIQAvD_BwE"},
-    {category: "Photographie", price: "60 €", name: "Flash pour appareil photo bas de gamme", rate: "4.2", lien: "https://www.amazon.fr/Neewer®-Speedlite-Affichage-Numérique-Standard/dp/B010XCEABO/ref=as_li_ss_tl?__mk_fr_FR=ÅMÅŽÕÑ&keywords=flash+neewer&qid=1581247623&s=electronics&sr=1-12&linkCode=sl1&tag=httpprogresen-21&linkId=88359fb94ba6e30b2eac540cd99512d4&language=fr_FR"},
+    {category: "Photographie", price: "400 €", name: "Objectif Nikkor 85mm", rate: "4.8", lien: "https://www.kamera-express.be/fr/nikon-af-s-85mm-f-1-8g-occasion-12428113"},
+    {category: "Photographie", price: "215 €", name: "Objectif Nikkor 50mm", rate: "4.5", lien: "https://www.amazon.fr/dp/B004Y1AYAC/ref=asc_df_B004Y1AYAC1639512000000/?tag=ledenicheur-amazon-fr-21&creative=22686&creativeASIN=B004Y1AYAC&linkCode=df0&smid=A1X6FK5RDHNB96&th=1"},
+    {category: "Photographie", price: "169 €", name: "Objectif Nikkor 35mm", rate: "4.3", lien: "https://www.amazon.fr/Nikon-Objectif-AF-S-DX-35/dp/B001S2PPT0"},
+    {category: "Photographie", price: "130 €", name: "Flash pour appareil photo millieu de gamme", rate: "4.5", lien: "https://www.photogalerie.com/flash-speedlite-yongnuo-yn-568ex-iii-pour-nikon?gclid=CjwKCAjwoP6LBhBlEiwAvCcthBMBmEE3jH5iKWEGHmToAoww86tWUCJTp2EOvifsz3TqQ-3lTZ9n4xoCcvIQAvD_BwE"},
+    {category: "Photographie", price: "40 €", name: "Flash pour appareil photo bas de gamme", rate: "4.2", lien: "https://www.amazon.fr/Neewer®-Speedlite-Affichage-Numérique-Standard/dp/B010XCEABO/ref=as_li_ss_tl?__mk_fr_FR=ÅMÅŽÕÑ&keywords=flash+neewer&qid=1581247623&s=electronics&sr=1-12&linkCode=sl1&tag=httpprogresen-21&linkId=88359fb94ba6e30b2eac540cd99512d4&language=fr_FR"},
     {category: "Photographie", price: "30 à 70 €", name: "Filtres 67mm ou 52mm (si objecif 35mm) ou 58mm (si objectif 50mm)", rate: "3", lien: "https://coolblue.be ou https://amazon.fr"},
-    {category: "Burreau", price: "85 €", name: "Souris sans fils Logitech G502", rate: "4.9", lien: "https://www.amazon.fr/Logitech-G502-Lightspeed-Programmables-Ordinateur/dp/B07QKC4WWD/ref=psdc_430335031_t2_B07Y8QWZDW?th=1"},
-    {category: "Alcool", price: "50 €", name: "Don papa", rate: "4", lien: "David Sanna"},
-    {category: "Alcool", price: "10 à 50 €", name: "Vin rouge", rate: "3", lien: "Delhaize"},
+    {category: "Alcool", price: "50 €", name: "Rhum Diplomatico", rate: "4", lien: "Luxembourg moins cher"},
+    {category: "Alcool", price: "10 à 50 €", name: "Vin rouge", rate: "3", lien: "Cellier"},
     {category: "Alcool", price: "15 à 40 €", name: "Verre à bière/rhum", rate: "4", lien: "https://amazon.fr ou Andrien"},
     {category: "Décoration", price: "30 €", name: "Déco mural avec une photo que j'ai réalisé", rate: "4.6", lien: "https://www.myposter.be/fr/"},
-    {category: "Décoration", price: "20 à 100 €", name: "Figurine d'un animé (one piece, naruto, demon slayer, hunter x hunter, tokyo revenger)", rate: "4.1", lien: "https://amazon.fr ou https://mangahako.com"},
-    {category: "Décoration", price: "40 €", name: "cadre d'un animé (one piece, naruto, hunter x hunter, tokyo revenger)", rate: "4.6", lien: "https://displate.com"},
+    {category: "Décoration", price: "20 à 100 €", name: "Figurine d'un animé (one piece, naruto, demon slayer, hunter x hunter, tokyo revenger, fullmetal alchemist)", rate: "4.1", lien: "https://amazon.fr ou https://mangahako.com"},
+    {category: "Décoration", price: "40 €", name: "cadre d'un animé (one piece, naruto, hunter x hunter, tokyo revenger, fullmetal alchemist)", rate: "4.6", lien: "https://displate.com"},
     {category: "Habits", price: "50 à 100 €", name: "Chaussures", rate: "4", lien: ""},
     {category: "Habits", price: "20 à 80 €", name: "Pull", rate: "3.9", lien: ""},
     {category: "Habits", price: "20 à 80 €", name: "Chemise", rate: "3.9", lien: ""},
     {category: "Habits", price: "30 à 80 €", name: "Pantalon jeans", rate: "4", lien: ""},
     {category: "Habits", price: "10 à 30 €", name: "Short de sport", rate: "3.8", lien: ""},
-    {category: "Activités", price: "200 €", name: "Saut en parachute", rate: "4.3", lien: "https://www.skydivespa.be/fr/offrir"},
-    {category: "Activités", price: "50 à 100 €", name: "Tour de circuit avec un pilote", rate: "4.7", lien: "https://www.sport-decouverte.com/bapteme-pilotage-voiture-de-sport.html"},
     {category: "Activités", price: "130 €", name: "Place pour un grand de F1", rate: "5", lien: ""},
     {category: "Activités", price: "25 à 50 €", name: "Karting entre amis", rate: "4.55", lien: "Eupen"},
-    {category: "Activités", price: "? €", name: "Weekend entre amis", rate: "4.95", lien: ""},
-    {category: "Activités", price: "? €", name: "Weekend avec Lara", rate: "4.95", lien: ""},
-    {category: "Autres", price: "7 €", name: "Manga: Demon slayer tome 2 (et suite), jujutsu kaisen tome 1 (et suite), one piece tome 4 (et suite)", rate: "3.8", lien: "https://www.fnac.be"},
+    {category: "Autres", price: "7 €", name: "Manga : https://www.listy.fr/l/73338/peinrr", rate: "4.6", lien: "https://www.fnac.be ; Club ; La traversée BD et manga (vervier)"},
     {category: "Autres", price: "10 à 30 €", name: "Jeu de société", rate: "3.78", lien: ""},
-    {category: "Autres", price: "10 à 30 €", name: "Jeu à boire", rate: "3", lien: ""},
-    {category: "Autres", price: "10 à 30 €", name: "Casse tête en bois", rate: "3", lien: ""}
+    {category: "Autres", price: "10 à 30 €", name: "Casse tête en bois", rate: "3", lien: ""},
+    {category: "Autres", price:"34€", name: "Coque de téléphone", rate: "4.5", lien: "https://rhinoshield.fr/collections/artist-one-piece?device=samsung-galaxy-s22&type=solidsuit-android&page=0&limit=12&dcolor=Black&bcolor=black&prod=samsung-galaxy-s22-solidsuit-android-om74"},
+
 ];
 
 function ProductRow ({products}) {
@@ -36,7 +32,6 @@ function ProductRow ({products}) {
         if(tabLien[i].indexOf("https://") >= 0)
             tabLien[i] = <a href={tabLien[i]} target="_blank">{tabLien[i]}</a>
     }
-
     let tabFinal = []
     for (let i = 0; i < tabLien.length; i++) {
         const element = tabLien[i]
@@ -44,12 +39,26 @@ function ProductRow ({products}) {
         tabFinal[(i*2)+1] = " "
     }
 
+    let tabName = products.lien.split(" ")
+    for(let i = 0; i < tabName.length; i++)
+    {
+        if(tabName[i].indexOf("https://") >= 0)
+        tabName[i] = <a href={tabName[i]} target="_blank">{tabName[i]}</a>
+    }
+    let tabNameFinal = []
+    for (let i = 0; i < tabName.length; i++) {
+        const element = tabName[i]
+        tabNameFinal[i*2] = element
+        tabNameFinal[(i*2)+1] = " "
+    }
+
+
     const starPercentage = (products.rate / 5) * 100
     const starPercentageRounded = `${Math.round(starPercentage /10) * 10}%`
     
     return <tr>
             <td>{products.category}</td>
-            <td>{products.name}</td>
+            <td>{tabNameFinal}</td>
             <td>{products.price}</td>
             <td>
                 <div className="stars-outer">
